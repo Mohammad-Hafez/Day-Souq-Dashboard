6c546edf4d62c46f6bfef90f4caebd2278db8d83
Mohamed Hafez <mhafez296@gmail.com>
2024-02-23 06:07:09 +0200

    change home layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,6 +11,9 @@ export default function Home() {
     Authorization: `Bearer ${user}`,
   };
 
+  const getAllBiddings = () => axios.get(ApiBaseUrl + 'products?isAction=true&dashboard=true', { headers });
+  const { data: biddingCount, isLoading: BiddingsLoading , error: biddingQuantityError } = useQuery('get Biddings', getAllBiddings, { cacheTime: 10000 });
+
   const getAllUsers = () => axios.get(ApiBaseUrl + 'users', { headers });
   const { data: userCount, isLoading: usersLoading } = useQuery('get users', getAllUsers, { cacheTime: 10000 });
 
@@ -30,7 +33,7 @@ export default function Home() {
       <Helmet>
         <title>Home</title>
       </Helmet>
-      {usersLoading || productQuantityLoading || orderLoading? (
+      {usersLoading || productQuantityLoading || BiddingsLoading || orderLoading? (
         <Loader/>
       ) : (
         <div className="container d-flex flex-column align-items-center text-center justify-content-center my-3">
@@ -38,14 +41,14 @@ export default function Home() {
             Welcome In Day Sooq Dashboard
           </h1>
           <div className="homeContent w-100">
-            <div className="row">
-              <div className="col-md-4">
+            <div className="row g-3">
+              <div className="col-md-3">
                 <div className="users shadow-sm rounded p-3 bg-light h-100 text-center">
                   <p className="numOfUsers fs-3 fw-bolder p-3 text-center rounded-circle mx-auto">{userCount?.data?.numOfDocs}</p>
                   <h3 className="fs-4 fw-bold my-2">Website Users</h3>
                 </div>
               </div>
-              <div className="col-md-4">
+              <div className="col-md-3">
                 {productQuantityError ? (
                   <span className="text-center">Error fetching product quantity</span>
                 ) : (
@@ -57,7 +60,7 @@ export default function Home() {
                   </div>
                 )}
               </div>
-              <div className="col-md-4">
+              <div className="col-md-3">
                 {orderQuantityError ? (
                   <span className="text-center">Error fetching orders quantity</span>
                 ) : (
@@ -66,6 +69,18 @@ export default function Home() {
                       {orderCount?.data?.numOfDocs}
                     </p>
                     <h3 className="fs-4 fw-bold my-2">All orders</h3>
+                  </div>
+                )}
+              </div>
+              <div className="col-md-3">
+                {biddingQuantityError ? (
+                  <span className="text-center">Error fetching All Biddings quantity</span>
+                ) : (
+                  <div className="products shadow-sm rounded p-3 bg-light h-100">
+                    <p className="fs-3 productsQuantity fw-bolder p-3 text-center rounded-circle mx-auto">
+                      {biddingCount?.data?.numOfDocs}
+                    </p>
+                    <h3 className="fs-4 fw-bold my-2">Products on Auction Now</h3>
                   </div>
                 )}
               </div>
