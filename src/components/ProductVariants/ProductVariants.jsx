feb0c46f781e388924e48f8235374929f0edc959
Mohamed Hafez <mhafez296@gmail.com>
2024-02-24 06:06:54 +0200

    add/edit/delete variant

--- src/components/ProductVariants/ProductVariants.jsx
+++ src/components/ProductVariants/ProductVariants.jsx
@@ -22,6 +22,8 @@ export default function ProductVariants({headers}) {
   const [SelectedProducts, setSelectedProducts] = useState(null)
   const [LoaderBtn, setLoaderBtn] = useState(false)
   const [Products, setProducts] = useState()
+  const [AddError, setAddError] = useState(null)
+  const [EditError, setEditError] = useState(null)
 
   const fetchProductVariants = ()=> axios.get(ApiBaseUrl + `products/${productId}/variants`)
 
@@ -44,21 +46,19 @@ export default function ProductVariants({headers}) {
     sku : "",
   }
   let validationSchema = Yup.object().shape({
-    quantity :Yup.string().required('quantity Is Required') ,
+    quantity :Yup.number().required('quantity Is Required') ,
     size :Yup.string().required('size Is Required') ,
     color :Yup.string().required('color Is Required') ,
-    extraPrice :Yup.string().required('extraPrice Is Required') ,
-    sku :Yup.string().required('sku Is Required') ,
-    imageCover: Yup.mixed().required('imageCover Is Required').test( 'fileSize', 'Image file size must be less than 2MB',
-      (value) => value && value.size <= 2097152, // 2MB in bytes
-    ),
+    extraPrice :Yup.number().required('extraPrice Is Required') ,
+    sku :Yup.number().required('sku Is Required') ,
+    imageCover: Yup.mixed().required('imageCover Is Required'),
     images: Yup.mixed().required('images Is Required')
   })
 
   let addFormik = useFormik({
     initialValues :initialVariant ,
     validationSchema , 
-
+    onSubmit:(values)=> addVariant(values)
   })
 
   let editFormik = useFormik({
@@ -84,6 +84,7 @@ export default function ProductVariants({headers}) {
       setLoaderBtn(false)
       hideDialog()
       }).catch(error => {
+        setAddError(error.response.data.message)
       console.error(error);
       setLoaderBtn(false)
     })
@@ -108,12 +109,12 @@ export default function ProductVariants({headers}) {
   const editVariant =  (id, values) => {
     setLoaderBtn(true)
     const formData = new FormData();
-    formData.append('quantity', values.quantity);
+    formData.append('product', productId);
+    // formData.append('quantity', values.quantity);
     formData.append('imageCover', values.imageCover);
     formData.append('size', values.size);
     formData.append('color', values.color);
     formData.append('images', values.images);
-    formData.append('sku', values.sku);
     formData.append('extraPrice', values.extraPrice);
     axios.patch(ApiBaseUrl + `variants/${id}`, formData, { headers })
     .then( response =>{
@@ -121,6 +122,7 @@ export default function ProductVariants({headers}) {
       setLoaderBtn(false)
       hideDialog()
     }).catch (error => {
+      setEditError(error.response.data.message)
       console.error(error);
       setLoaderBtn(false)
     })
@@ -153,7 +155,7 @@ export default function ProductVariants({headers}) {
     );
   };
   // *ANCHOR - image format 
-  const variantAllImages = (rowData) => rowData?.images?.map((image , index) => <img key={index} src={ImgBaseURL + image } alt={rowData.name + 'image'} className='w-50' />).slice(0,1)
+  const variantAllImages = (rowData) => rowData?.images?.map((image , index) => <img key={index} src={ImgBaseURL + image } alt={rowData.name + 'image'} className='w-25' />).slice(0,1)
   const variantImage = (rowData) => <img src={ImgBaseURL + rowData.imageCover } alt={rowData.name + 'image'} className='w-50' />
   
   const ProductsHeaderBody = ()=>{
@@ -178,7 +180,7 @@ export default function ProductVariants({headers}) {
         <Loader/>
       </div> :<>
           <DataTable  value={Products}  header={ProductsHeaderBody} paginator selectionMode="single" className={`dataTabel mb-4 text-capitalize AllList`} dataKey="_id" scrollable scrollHeight="100vh" tableStyle={{ minWidth: "50rem" }} rows={10} responsive="scroll">
-            <Column header="All Images" body={variantAllImages} style={{ width: "10%", borderBottom: '1px solid #dee2e6' }} />
+            <Column header="All Images" body={variantAllImages} style={{ width: "20%", borderBottom: '1px solid #dee2e6' }} />
             <Column header="Image" body={variantImage} style={{ width: "10%", borderBottom: '1px solid #dee2e6' }} />
             <Column field="quantity" header="quantity" sortable style={{ width: "10%", borderBottom: '1px solid #dee2e6' }} />
             <Column header="size" field='size' sortable style={{ width: "5%", borderBottom: '1px solid #dee2e6' }} />
@@ -194,7 +196,94 @@ export default function ProductVariants({headers}) {
             <button className='btn btn-danger  w-50 mx-2 px-4' onClick={()=>{deleteCategory(SelectedProducts)}}>Yes</button>
             <button className='btn btn-primary w-50 mx-2 px-4' onClick={()=>{setDisplayDeleteDialog(false)}}>No</button>
             </div>
-          </Dialog>           
+          </Dialog>   
+          <Dialog header={'Edit Variant'} className='container editDialog' visible={displayEditDialog} onHide={hideDialog} modal>
+            <form onSubmit={editFormik.handleSubmit} className='bg-light p-3 border shadow-sm rounded'>
+              {/* <div className="form-floating mb-2">
+                <input type="number" placeholder='Quantity' className="form-control" id="quantity" name="quantity" value={editFormik.values.quantity} onChange={editFormik.handleChange} onBlur={editFormik.handleBlur} />
+                <label className='ms-2' htmlFor="quantity">Quantity</label>
+                {editFormik.errors.quantity && editFormik.touched.quantity ? (<div className="alert text-danger">{editFormik.errors.quantity}</div>) : null}
+              </div> */}
+              <div className="form-floating mb-2">
+                <input type="text" placeholder='Size' className="form-control" id="size" name="size" value={editFormik.values.size} onChange={editFormik.handleChange} onBlur={editFormik.handleBlur} />
+                <label className='ms-2' htmlFor="size">Size</label>
+                {editFormik.errors.size && editFormik.touched.size ? (<div className="alert text-danger">{editFormik.errors.size}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="text" placeholder='Color' className="form-control" id="color" name="color" value={editFormik.values.color} onChange={editFormik.handleChange} onBlur={editFormik.handleBlur} />
+                <label className='ms-2' htmlFor="color">Color</label>
+                {editFormik.errors.color && editFormik.touched.color ? (<div className="alert text-danger">{editFormik.errors.color}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="number" placeholder='Extra Price' className="form-control" id="extraPrice" name="extraPrice" value={editFormik.values.extraPrice} onChange={editFormik.handleChange} onBlur={editFormik.handleBlur} />
+                <label className='ms-2' htmlFor="extraPrice">Extra Price</label>
+                {editFormik.errors.extraPrice && editFormik.touched.extraPrice ? (<div className="alert text-danger">{editFormik.errors.extraPrice}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="file" className="form-control" id="imageCover" name="imageCover" onChange={(event) => {editFormik.setFieldValue("imageCover", event.currentTarget.files[0])}} onBlur={editFormik.handleBlur} />
+                <label className='ms-2 mb-3 pt-2 h-auto p-0' htmlFor="imageCover">Image Cover</label>
+                {editFormik.errors.imageCover && editFormik.touched.imageCover ? (<div className="alert text-danger">{editFormik.errors.imageCover}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="file" className="form-control" id="images" name="images" multiple onChange={(event) => {editFormik.setFieldValue("images", event.currentTarget.files)}} onBlur={editFormik.handleBlur} />
+                <label className='ms-2 mb-3 pt-2 h-auto p-0' htmlFor="images">Images</label>
+                {editFormik.errors.images && editFormik.touched.images ? (<div className="alert text-danger">{editFormik.errors.images}</div>) : null}
+              </div>
+              {EditError ? <div className='alert text-danger'>{EditError}</div> :null}
+
+              <div className="btns ms-auto w-100 d-flex justify-content-center mt-3">
+            {LoaderBtn ? <button className='btn btn-primary text-light w-50' disabled><i className="fa fa-spin fa-spinner"></i></button> :
+              <Button label="SUBMIT" type="submit" icon="pi pi-check" disabled={!(editFormik.isValid && editFormik.dirty)} className="btn btn-primary text-light w-50" />
+            }
+          </div>
+            </form>
+          </Dialog>
+          <Dialog header={'Add New Variant'} className='container editDialog' visible={displayAddNewDialog} onHide={hideDialog} modal>
+            <form onSubmit={addFormik.handleSubmit} className='bg-light p-3 border shadow-sm rounded'>
+              <div className="form-floating mb-2">
+                <input type="number" placeholder='Quantity' className="form-control" id="quantity" name="quantity" value={addFormik.values.quantity} onChange={addFormik.handleChange} onBlur={addFormik.handleBlur} />
+                <label className='ms-2' htmlFor="quantity">Quantity</label>
+                {addFormik.errors.quantity && addFormik.touched.quantity ? (<div className="alert text-danger">{addFormik.errors.quantity}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="number" placeholder='sku' className="form-control" id="sku" name="sku" value={addFormik.values.sku} onChange={addFormik.handleChange} onBlur={addFormik.handleBlur} />
+                <label className='ms-2' htmlFor="sku">sku</label>
+                {addFormik.errors.sku && addFormik.touched.sku ? (<div className="alert text-danger">{addFormik.errors.sku}</div>) : null}
+              </div>
+
+              <div className="form-floating mb-2">
+                <input type="text" placeholder='Size' className="form-control" id="size" name="size" value={addFormik.values.size} onChange={addFormik.handleChange} onBlur={addFormik.handleBlur} />
+                <label className='ms-2' htmlFor="size">Size</label>
+                {addFormik.errors.size && addFormik.touched.size ? (<div className="alert text-danger">{addFormik.errors.size}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="text" placeholder='Color' className="form-control" id="color" name="color" value={addFormik.values.color} onChange={addFormik.handleChange} onBlur={addFormik.handleBlur} />
+                <label className='ms-2' htmlFor="color">Color</label>
+                {addFormik.errors.color && addFormik.touched.color ? (<div className="alert text-danger">{addFormik.errors.color}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="number" placeholder='Extra Price' className="form-control" id="extraPrice" name="extraPrice" value={addFormik.values.extraPrice} onChange={addFormik.handleChange} onBlur={addFormik.handleBlur} />
+                <label className='ms-2' htmlFor="extraPrice">Extra Price</label>
+                {addFormik.errors.extraPrice && addFormik.touched.extraPrice ? (<div className="alert text-danger">{addFormik.errors.extraPrice}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="file" className="form-control" id="imageCover" name="imageCover" onChange={(event) => {addFormik.setFieldValue("imageCover", event.currentTarget.files[0]) ; console.log(event.currentTarget.files[0]);}} onBlur={addFormik.handleBlur} />
+                <label className='ms-2 mb-3 pt-2 h-auto p-0' htmlFor="imageCover">Image Cover</label>
+                {addFormik.errors.imageCover && addFormik.touched.imageCover ? (<div className="alert text-danger">{addFormik.errors.imageCover}</div>) : null}
+              </div>
+              <div className="form-floating mb-2">
+                <input type="file" className="form-control" id="images" name="images" multiple onChange={(event) => {addFormik.setFieldValue("images", event.currentTarget.files) ; console.log(event.currentTarget.files);}} onBlur={addFormik.handleBlur} />
+                <label className='ms-2 mb-3 pt-2 h-auto p-0' htmlFor="images">Images</label>
+                {addFormik.errors.images && addFormik.touched.images ? (<div className="alert text-danger">{addFormik.errors.images}</div>) : null}
+              </div>
+              {AddError ? <div className='alert text-danger'>{AddError}</div> :null}
+              <div className="btns ms-auto w-100 d-flex justify-content-center mt-3">
+            {LoaderBtn ? <button className='btn btn-primary text-light w-50' disabled><i className="fa fa-spin fa-spinner"></i></button> :
+              <Button label="SUBMIT" type="submit" icon="pi pi-check" disabled={!(addFormik.isValid && addFormik.dirty)} className="btn btn-primary text-light w-50" />
+            }
+          </div>
+            </form>
+          </Dialog>
           </>  
       }
     </div>
